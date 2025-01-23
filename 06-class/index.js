00bb2fc41e7bf43f87e71a5ccfb7f8c6f71cbb48
maddieposhard <poshardmadeline@gmail.com>
2025-01-23 15:35:36 -0600

    working on homework exercises 1-5

---
+++ 06-class/index.js
@@ -0,0 +1,59 @@
+/*
+console.log('im connected');
+
+const person = {
+    name: "Maddie Poshard",
+    age: 31,
+    greet: function() {
+        console.log('Hello!');
+
+    }
+}
+
+console.log(person);
+*/
+
+
+//exercise 1
+const car = {
+    make: 'Kia',
+    model: 'Sportage',
+    year: 2024,
+    getInfo: function() {
+        console.log(`Make:${this.make} Model:${this.model} Year:${this.year}`);
+    }
+};
+
+    car.getInfo();
+
+//exercise 2
+const student = {
+    name: 'Maddie',
+    age: 31,
+    grades: [100, 75, 50],
+    calculateAverageGrade: function(grades) {
+        const average = grades.reduce(function(sum, num) {return sum + num} , 0) / grades.length;
+        console.log(`${this.name}'s average grade is ${average}`);
+    }
+}
+
+student.calculateAverageGrade(student.grades);
+
+//exercise 3
+const element = document.getElementById('myDiv');
+const button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+    element.style.color = 'red';
+  });
+console.log(element)
+
+//exercise 4
+const btn = document.getElementById('myBtn');
+const paragraph = document.getElementById('myPara');
+btn.addEventListener('click', function() {
+  const myPara = document.getElementById('myPara');
+  const newPara = document.createElement('newPara');
+  newPara.textContent = 'This is a new paragraph';
+  paragraph.appendChild(newPara);
+});
+
